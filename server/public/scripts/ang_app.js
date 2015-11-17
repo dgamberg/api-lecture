1b3ad388bcbd53bc2da6b81f3e8f719a694fe225
Dave Gamberg <dgamberg@gmail.com>
2015-11-17 16:10:24 -0600

    another commit

--- server/public/scripts/ang_app.js
+++ server/public/scripts/ang_app.js
@@ -1,18 +1,19 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
-    var apikey = "ca5d63fdb921e2570069eb8267389c4a9260227a";
+    var apikey = "aa3066b97cf3e38461c8b09cc5f4b595daa7f6b8";
     var baseUrl = "http://www.giantbomb.com/api";
 
     // construct our URL
     var gameSearchURL = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp&limit=50';
-    var query = 'Batman & Robin';
+    var query = 'Superman';
 
     var finalURL = gameSearchURL + '&query=' + encodeURI(query) + '&json_callback=JSON_CALLBACK';
-
+    var responseArray = [];
     $http.jsonp(finalURL).then(
         function(response) {
-            console.log(response.data);
+            responseArray = response.data;
+            console.log(responseArray);
         }
     );
 
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 });
 
 function gameSearch() {
-    var apikey = "ca5d63fdb921e2570069eb8267389c4a9260227a";
+    var apikey = "aa3066b97cf3e38461c8b09cc5f4b595daa7f6b8";
     var baseUrl = "http://www.giantbomb.com/api";
 
     // construct our URL
     var gameSearchURL = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp&limit=50';
-    var query = 'Batman & Robin';
+    var query = 'Superman';
 
     var q = gameSearchURL + '&query=' + encodeURI(query);
     console.log(q);
@@ -20,12 +20,24 @@ function gameSearch() {
         crossDomain: true,
         jsonp: 'json_callback',
         success: function(data) {
-            console.log(data);
+            updateDOM(data);
+            console.log(data.results);
         }
     });
 
 }
 
-function searchCallback(data) {
-    console.log(data);
+
+function updateDOM(data){
+    for(var i = 0; i < data.results.length; i++){
+        var output = "<div class='well'>" +
+                "<h3>" + data.results[i].name +  "</h3>" +
+                "<h5>" + data.results[i].deck + "</h5>" +
+                "<p>" + data.results[i].original_release_date + "</p>" +
+                "<p>" + data.results[i].description + "</p>" +
+                "</div>";
+
+
+        $('#outputContainer').append(output);
+    }
 }
